956e52eaaa1596894c15fd34d5c6493b77a37d59
pcheung <pika206@gmail.com>
2022-09-09 13:14:31 -0500

    add blackjack deck array, playerHand array, dealerHand array as game variables

--- js/app.js
+++ js/app.js
@@ -1,6 +1,65 @@
 
 // ----------------------------Constants----------------------------------------------
+
 // ----------------------------Variables (state)--------------------------------------
+//? once testing is good we can shrink this into one line.
+const blackJackDeck = [
+  {id:'dA', value:1},
+  {id:'dK', value:10},
+  {id:'dQ', value:10},
+  {id:'dJ', value:10},
+  {id:'d10', value:10},
+  {id:'d09', value:9},
+  {id:'d08', value:8},
+  {id:'d07', value:7},
+  {id:'d06', value:6},
+  {id:'d05', value:5},
+  {id:'d04', value:4},
+  {id:'d03', value:3},
+  {id:'d02', value:2},
+  {id:'hA', value:1},
+  {id:'hK', value:10},
+  {id:'hQ', value:10},
+  {id:'hJ', value:10},
+  {id:'h10', value:10},
+  {id:'h09', value:9},
+  {id:'h08', value:8},
+  {id:'h07', value:7},
+  {id:'h06', value:6},
+  {id:'h05', value:5},
+  {id:'h04', value:4},
+  {id:'h03', value:3},
+  {id:'h02', value:2},
+  {id:'cA', value:1},
+  {id:'cK', value:10},
+  {id:'cQ', value:10},
+  {id:'cJ', value:10},
+  {id:'c10', value:10},
+  {id:'c09', value:9},
+  {id:'c08', value:8},
+  {id:'c07', value:7},
+  {id:'c06', value:6},
+  {id:'c05', value:5},
+  {id:'c04', value:4},
+  {id:'c03', value:3},
+  {id:'c02', value:2},
+  {id:'sA', value:1},
+  {id:'sK', value:10},
+  {id:'sQ', value:10},
+  {id:'sJ', value:10},
+  {id:'s10', value:10},
+  {id:'s09', value:9},
+  {id:'s08', value:8},
+  {id:'s07', value:7},
+  {id:'s06', value:6},
+  {id:'s05', value:5},
+  {id:'s04', value:4},
+  {id:'s03', value:3},
+  {id:'s02', value:2},
+]
+
+let playerHand = []
+let dealerHand = []
 // ----------------------------Cached Element references------------------------------
 let messageEl = document.querySelector('#game-message')
 let drawBtn = document.querySelector('#draw-btn')
@@ -13,18 +72,8 @@ drawBtn.addEventListener('click', function(){
 // ----------------------------Functions----------------------------------------------
 
 
-//* examples of player hands for testing getHandValue
 
-let playerHand21 = [
-  {id:'sA', value:1}, 
-  {id:'dK', value:10},
-]
 
-let playerHand12 = [
-  {id:'sA', value:1},
-  {id:'cA', value:1}, 
-  {id:'dK', value:10},
-]
 
 
 
@@ -37,11 +86,23 @@ function getHandValue(handArr){
   } 
   return hasAce && total < 12 ? total + 10 : total 
 }
-console.log('playerHand 21', getHandValue(playerHand21))
-console.log('playerHand 12', getHandValue(playerHand12))
+
+//* tests commented out unless needed
+
+// let playerHand21 = [
+//   {id:'sA', value:1}, 
+//   {id:'dK', value:10},
+// ]
+
+// let playerHand12 = [
+//   {id:'sA', value:1},
+//   {id:'cA', value:1}, 
+//   {id:'dK', value:10},
+// ]
+
+// console.log('playerHand 21', getHandValue(playerHand21))
+// console.log('playerHand 12', getHandValue(playerHand12))
 // console.log('playerHand 25', getHandValue(playerHand25))
-
-
 
 
 
