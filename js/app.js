edf28a65f35bd8af196c4113b99dff38ea3a8e3e
pcheung <pika206@gmail.com>
2022-09-09 07:36:14 -0500

    app.js: new function to get the total value of a hand is working with sample data

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-console.log('san check')
+
 // ----------------------------Constants----------------------------------------------
 // ----------------------------Variables (state)--------------------------------------
 // ----------------------------Cached Element references------------------------------
@@ -6,8 +6,36 @@ console.log('san check')
 // ----------------------------Functions----------------------------------------------
 
 
+//* examples of player hands for testing getHandValue
+let playerHand21 = [
+  {suit:'spade', rank:1, value:1}, 
+  {suit:'spade', rank:13, value:10},
+]
+let playerHand12 = [
+  {suit:'spade', rank:1, value:1}, 
+  {suit:'spade', rank:1, value:1}, 
+  {suit:'spade', rank:13, value:10},
+]
+let playerHand25 = [
+  {suit:'spade', rank:3, value:3}, 
+  {suit:'diamond', rank:2, value:2}, 
+  {suit:'club', rank:10, value:10},
+  {suit:'heart', rank:11, value:10},
+]
 
 
+function getHandValue(handArr){
+  let hasAce = false
+  let total = 0 
+  for (let i=0;i<handArr.length; i++){
+    total += handArr[i].value
+    hasAce = handArr[i].value === 1 ? true : hasAce
+  } 
+  return hasAce && total < 12 ? total + 10 : total //if the hand has an ace and the hard total is under 12, then the bonus 10 value is applicable to the total
+}
+console.log('playerHand 21', getHandValue(playerHand21))
+console.log('playerHand 12', getHandValue(playerHand12))
+console.log('playerHand 25', getHandValue(playerHand25))
 
 
 
