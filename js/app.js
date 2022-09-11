25be171289cfc88cae09053678a0634b07e871f4
pcheung <pika206@gmail.com>
2022-09-11 07:52:53 -0500

    stub out new game button, hook up event listener

--- js/app.js
+++ js/app.js
@@ -66,6 +66,7 @@ let drawBtn = document.querySelector('#hit-btn')
 let standBtn = document.querySelector('#stand-btn')
 let playerHandDiv = document.querySelector('#player-hand')
 let dealerHandDiv = document.querySelector('#dealer-hand')
+let newGameBtn = document.querySelector('#new-game-btn')
 // ----------------------------Event Listeners----------------------------------------
 drawBtn.addEventListener('click', function(){
   if (turn === 'player-turn'){handleClickHit(playerHand)}
@@ -75,6 +76,9 @@ standBtn.addEventListener('click', function(){
   if (turn === 'player-turn'){handleClickStand()}
 })
 
+newGameBtn.addEventListener('click', function(){
+  console.log('click')
+})
 
 // ----------------------------Functions----------------------------------------------
 initialDeal()
